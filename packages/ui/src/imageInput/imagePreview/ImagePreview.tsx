35bc8460aec323826c374305730927cae6a39c10
hongggy <hongggy@gmail.com>
2025-03-12 16:50:28 +0900

    feat: 프리뷰 dnd

--- packages/ui/src/imageInput/imagePreview/ImagePreview.tsx
+++ packages/ui/src/imageInput/imagePreview/ImagePreview.tsx
@@ -3,39 +3,29 @@ import useImageUrlStore from "../../store/useImageUrlStore";
 import { UrlType } from "../../types/imageUrlType";
 import { GoXCircleFill } from "react-icons/go";
 import * as style from "./style.css";
-import { DragEvent } from "react";
 const ImagePreview = ({ url }: { url: UrlType }) => {
   const { deleteUrl } = useImageUrlStore();
   const handleDeleteImageFile = () => {
     deleteUrl(url.id);
   };
 
-  const handleDragImage = (e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
-
-  const handleDropImage = (e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
   return (
-    <div draggable onDragOver={handleDragImage} onDrop={handleDropImage}>
-      <InputWrapper>
-        <img
-          src={url.url}
-          alt="미리보기"
-          width={50}
-          height={50}
-          draggable={false}
-        />
-        <button
-          type="button"
-          onClick={handleDeleteImageFile}
-          className={style.deleteButton}
-        >
-          <GoXCircleFill />
-        </button>
-      </InputWrapper>
-    </div>
+    <InputWrapper>
+      <img
+        src={url.url}
+        alt="미리보기"
+        width={50}
+        height={50}
+        draggable={false}
+      />
+      <button
+        type="button"
+        onClick={handleDeleteImageFile}
+        className={style.deleteButton}
+      >
+        <GoXCircleFill />
+      </button>
+    </InputWrapper>
   );
 };
 
