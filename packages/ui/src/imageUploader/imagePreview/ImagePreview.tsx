b211cc04bf7cb2518ca5c36d5e762baa3dda716c
Yelim Hong <yelimhong@Yelims-MacBook-Pro.local>
2025-03-17 09:43:26 +0900

    fix: 빌드 에러

---
+++ packages/ui/src/imageUploader/imagePreview/ImagePreview.tsx
@@ -0,0 +1,27 @@
+import { GoXCircleFill } from "react-icons/go";
+import useImageUrlStore from "../../../store/useImageUrlStore";
+import InputWrapper from "../../inputWrapper/InputWrapper";
+import type { UrlType } from "../../types/imageUrlType";
+import * as style from "./style.css";
+
+const ImagePreview = ({ url }: { url: UrlType }) => {
+  const { deleteUrl } = useImageUrlStore();
+  const handleDeleteImageFile = () => {
+    deleteUrl(url.id);
+  };
+
+  return (
+    <InputWrapper>
+      <img src={url.url} alt="미리보기" width={50} height={50} />
+      <button
+        type="button"
+        onClick={handleDeleteImageFile}
+        className={style.deleteButton}
+      >
+        <GoXCircleFill />
+      </button>
+    </InputWrapper>
+  );
+};
+
+export default ImagePreview;
