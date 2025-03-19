3a977c0d5bd133fae0fe95f2c3240acfef82b8a7
yelim <hongggy@gmail.com>
2025-03-19 14:09:16 +0900

    chore: vite config 수정

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,14 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // alias 설정 추가
-  resolve: {
-    alias: {
-      "@repo/ui": path.resolve(__dirname, "../../packages/ui/src"),
-    },
-  },
 });
