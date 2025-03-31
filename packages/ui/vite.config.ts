db88ef11c3bd31489657f13853e3db7d0f7570db
yelim <hongggy@gmail.com>
2025-03-31 19:51:29 +0900

    fix: conflict

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     vanillaExtractPlugin({}) as any,
     dts(),
     svgr({
-      include: "**/*.svg?react, **/*.svg",
+      include: "**/*.svg",
       exclude: "",
     }),
     {
