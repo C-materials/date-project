200022b7fb61788a724f68582c225658e18c6d75
yelim <hongggy@gmail.com>
2025-04-03 11:44:25 +0900

    chore: switch div 삭제

--- packages/ui/src/components/switch/Switch.tsx
+++ packages/ui/src/components/switch/Switch.tsx
@@ -12,21 +12,19 @@ const Switch = ({
   ...props
 }: CheckBoxProps) => {
   return (
-    <div onClick={onClick}>
-      <label htmlFor={id} className={switchStyle({ disabled, checked })}>
-        <span className={toggle({ disabled, checked })}></span>
+    <label htmlFor={id} className={switchStyle({ disabled, checked })}>
+      <span className={toggle({ disabled, checked })}></span>
 
-        <input
-          type="checkbox"
-          checked={checked}
-          value={name}
-          id={id}
-          disabled={disabled}
-          onChange={onChange}
-          {...props}
-        />
-      </label>
-    </div>
+      <input
+        type="checkbox"
+        checked={checked}
+        value={name}
+        id={id}
+        disabled={disabled}
+        onChange={onChange}
+        {...props}
+      />
+    </label>
   );
 };
 
