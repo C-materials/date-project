91dc950fca886530d8fa6a0d52e8f89ed66217e9
yelim <hongggy@gmail.com>
2025-04-09 11:11:27 +0900

    feat: nextjs vanilla extract config추가

--- apps/user/src/app/components/header/Header.tsx
+++ apps/user/src/app/components/header/Header.tsx
@@ -1,22 +1,23 @@
 "use client";
 import { Button } from "@repo/ui";
 import Image from "next/image";
+import { header, itemWrapper, logoWrapper } from "./style.css";
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className={header}>
+      <div className={logoWrapper}>
         <Image
           src="/apps/user/public/logo.png"
           width={81}
           height={26}
           alt="&DEAR"
         />
-        <ul>
+        <ul className={itemWrapper}>
           <li>menu</li>
           <li>menu</li>
         </ul>
       </div>
-      <div>
+      <div className={itemWrapper}>
         <button type="button">Action</button>
         <Button type="button" variant="outline">
           Button
--- apps/user/src/app/components/header/style.css.ts
+++ apps/user/src/app/components/header/style.css.ts
@@ -1,29 +1,29 @@
-// import { style } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 // import { Color } from "../../../../../../packages/ui/src/styles";
 
-// export const header = style({
-//   height: "72px",
-//   width: "100%",
-//   padding: "0 40px",
+export const header = style({
+  height: "72px",
+  width: "100%",
+  padding: "0 40px",
 
-//   display: "flex",
-//   justifyContent: "space-between",
-//   alignItems: "center",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 
-//   background: Color.bg,
-//   color: Color.text.default,
-//   borderBottom: "1px solid",
-//   borderColor: Color.border,
-// });
+  // background: Color.bg,
+  // color: Color.text.default,
+  borderBottom: "1px solid",
+  // borderColor: Color.border,
+});
 
-// export const itemWrapper = style({
-//   display: "flex",
-//   gap: "12px",
-// });
+export const itemWrapper = style({
+  display: "flex",
+  gap: "12px",
+});
 
-// export const logoWrapper = style({
-//   display: "flex",
-//   gap: "40px",
-//   justifyContent: "space-between",
-//   alignItems: "center",
-// });
+export const logoWrapper = style({
+  display: "flex",
+  gap: "40px",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
