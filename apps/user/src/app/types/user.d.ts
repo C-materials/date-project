e0494662fa6540915a880a37be97ac6245f22c59
박종준 <poiufgin7373@naver.com>
2025-04-11 16:51:45 +0900

    types: user namespace 정의

---
+++ apps/user/src/app/types/user.d.ts
@@ -0,0 +1,19 @@
+namespace User {
+  type Info = {
+    id: number;
+    name: string;
+    tel: string;
+    approved: boolean;
+    birthYear: string;
+    birthMonth: string;
+    birthDay: string;
+    birth: Date;
+    address: string;
+    gender: Gender;
+  };
+
+  type FormValue = {
+    password: string;
+    passwordCheck: string;
+  } & Omit<Info, "id" | "approved">;
+}
