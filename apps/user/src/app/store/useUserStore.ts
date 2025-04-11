6cb9ccc6af1d288789269eab2802dc93bcbcc19e
yelim <hongggy@gmail.com>
2025-04-11 12:30:31 +0900

    feat: zustand user store

---
+++ apps/user/src/app/store/useUserStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+interface User {
+  id: number | null;
+  name: string | null;
+  tel: string | null;
+}
+interface UserStore {
+  user: User;
+  setUser: () => void;
+  setLogout: () => void;
+}
+const useStore = create<UserStore>((set) => ({
+  user: {
+    id: null,
+    name: null,
+    tel: null,
+  },
+  setUser: () => set({ user: { id: 1, name: "홍길동", tel: "01012341234" } }),
+  setLogout: () => set({ user: { id: null, name: null, tel: null } }),
+}));
