15977abec3c4e61cbe06c331e13fc7d2834a4b38
yelim <hongggy@gmail.com>
2025-04-11 22:44:57 +0900

    type: select 옵션 타입 수정

--- packages/ui/src/components/select/type.ts
+++ packages/ui/src/components/select/type.ts
@@ -1,7 +1,7 @@
 import type { InputProps } from "../textfield/textInput/type";
 
 export type SelectProps = InputProps & {
-  optionList: string[];
+  optionList: string[] | number[];
   onChangeValue: (value: string) => void;
   isOpen?: boolean;
   onClickClose?: () => void;
