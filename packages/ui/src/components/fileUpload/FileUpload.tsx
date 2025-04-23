a9f2ca9156c5d9e7afb52b180bea9466f8b0ff3b
yelim <hongggy@gmail.com>
2025-04-23 14:53:05 +0900

    refactor: File 형태로 상위로 전달

--- packages/ui/src/components/fileUpload/FileUpload.tsx
+++ packages/ui/src/components/fileUpload/FileUpload.tsx
@@ -4,52 +4,26 @@ import Upload from "../../../assets/uploadIcon.svg";
 import Button from "../button/Button";
 import { notDisplay } from "../checkBox/style.css";
 import { button, icon, labelStyle, wrapper } from "./style.css";
+import type { ImageType } from "./type";
 
 type FileProps = Omit<ComponentPropsWithoutRef<"input">, "onChange"> & {
-  maxLength?: number;
-  maxSize?: number;
-  urlList: any[];
+  urlList: ImageType[];
   acceptFormatList?: string;
-  onChange: (files: any[]) => void;
+  onChange: (files: FileList) => void;
 };
 const FileUpload = ({
   disabled = false,
   onChange,
-  maxLength = 5,
-  maxSize = 5,
   urlList,
   acceptFormatList = "image/jpg, image/png, image/jpeg",
   ...props
 }: FileProps) => {
   const [isDropActive, setIsDropActive] = useState(false);
 
-  const LIMIT_SIZE = maxSize * 1024 * 1024;
-
-  //  상위에서 파일 리스트 업데이트하는 함수
-  const uploadFiles = (files: FileList) => {
-    if (urlList.length + files.length > maxLength) {
-      alert(`사진은 최대 ${maxLength}장 등록 가능합니다.`);
-      return [];
-    }
-    const filteredFiles = Array.from(files).filter(
-      (file) => file.size < LIMIT_SIZE,
-    );
-    if (filteredFiles.length <= 0) {
-      alert(`${maxSize}MB 이하의 파일만 업로드 가능합니다`);
-      return [];
-    }
-
-    return filteredFiles.map((file) => ({
-      id: `${file.lastModified} + ${file.name}`,
-      url: URL.createObjectURL(file),
-    }));
-  };
-
   const handleSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (!files) return;
-    const uploadedFiles = uploadFiles(files);
-    onChange?.(uploadedFiles);
+    onChange?.(files);
   };
 
   const handleDragStart = (e: React.DragEvent) => {
@@ -72,8 +46,7 @@ const FileUpload = ({
     setIsDropActive(false);
 
     const files = e.dataTransfer.files;
-    const uploadedFiles = uploadFiles(files);
-    onChange?.(uploadedFiles);
+    onChange?.(files);
   };
 
   const inputRef = useRef<HTMLInputElement>(null);
