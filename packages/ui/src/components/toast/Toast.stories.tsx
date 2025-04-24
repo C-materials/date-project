2f6033a31b2edccbd4d94f077cb3285d23433e96
yelim <hongggy@gmail.com>
2025-04-24 11:27:20 +0900

    feat: Toast 컴포넌트 개발

---
+++ packages/ui/src/components/toast/Toast.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { story } from "../../styles/storyStyle.css";
+import Toast from "./Toast";
+
+const meta: Meta<typeof Toast> = {
+  component: Toast,
+  title: "UI/Toast",
+  argTypes: {
+    type: {
+      description: `"success" | "warning" | "critical"`,
+      control: "radio",
+      options: ["success", "warning", "critical"],
+    },
+    children: {
+      description: "토스트 내부 컨텐츠",
+    },
+    icon: {
+      description: "아이콘 추가 여부",
+      control: "boolean",
+    },
+    top: {
+      description: "토스트 상단으로부터의 위치",
+      control: "range",
+    },
+    width: {
+      description: "토스트 너비",
+      control: { type: "range", min: 240, max: 400 },
+    },
+    className: {
+      description: "토스트에 적용할 스타일",
+    },
+    isOpen: {
+      description: "토스트 노출 여부",
+      control: "boolean",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toast>;
+
+export const Default: Story = {
+  args: {
+    type: "success",
+    icon: true,
+    top: 64,
+    width: 240,
+    isOpen: true,
+  },
+  render: (args) => {
+    return (
+      <div className={story}>
+        <Toast {...args} />
+      </div>
+    );
+  },
+};
---
+++ packages/ui/src/components/toast/Toast.tsx
@@ -0,0 +1,62 @@
+import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import Error from "../../../assets/error.svg";
+import Success from "../../../assets/success.svg";
+import Warning from "../../../assets/warning.svg";
+import { iconStyle, toast } from "./style.css";
+type ToastType = {
+  type: "success" | "warning" | "critical";
+  children: ReactNode;
+  icon?: boolean;
+  top?: number;
+  isOpen: boolean;
+  width?: number;
+  className?: any;
+};
+
+const Toast = ({
+  isOpen,
+  type,
+  children = "Toast Text",
+  icon,
+  top,
+  width,
+  className,
+}: ToastType) => {
+  const [isVisible, setIsVisible] = useState(isOpen);
+  const iconType = {
+    success: <Success className={iconStyle} />,
+    warning: <Warning className={iconStyle} />,
+    critical: <Error className={iconStyle} />,
+  };
+
+  useEffect(() => {
+    if (isOpen) setIsVisible(true);
+
+    let timeout: NodeJS.Timeout | null = null;
+    if (!isOpen) {
+      timeout = setTimeout(() => setIsVisible(false), 300);
+    }
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [isOpen]);
+
+  if (!isVisible) return null;
+
+  return (
+    <>
+      <div
+        className={`${toast({ type, isOpen })} ${className || ""}`}
+        style={{ top: top ?? "64px", width: width || "160px" }}
+      >
+        {icon && iconType[type]}
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Toast;
